21866cd4b59b8ca64fcc399bd1f8a5bcc3644016
Nicholas Wagner <tinker@hackbookpro.local>
2016-01-30 10:39:41 -0600

    adds blanket assets dir

---
+++ Assets/Scripts/Bullet.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Bullet : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnCollisionEnter(Collision collision)
+	{
+		var hit = collision.gameObject;
+		var hitPlayer = hit.GetComponent<PlayerMove> ();
+		if (hitPlayer != null) {
+			Destroy (gameObject);
+		}
+	}
+}
---
+++ Assets/Scripts/PlayerMove.cs
@@ -0,0 +1,56 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.Networking;
+
+public class PlayerMove : NetworkBehaviour {
+
+	public GameObject bulletPrefab;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (!isLocalPlayer)
+			return;
+		
+		var x = Input.GetAxis ("Horizontal") * 0.1f;
+		var z = Input.GetAxis ("Vertical") * 0.1f;
+
+		transform.Translate (x, 0, z);
+
+		if (Input.GetKeyDown (KeyCode.Space)) {
+			CmdFire ();
+		}
+	}
+
+	[Command]
+	void CmdFire()
+	{
+		// This [Command] code runs on the server!
+
+		// create the bullet object from the bullet prefab
+		var bullet = (GameObject)Instantiate (
+			             bulletPrefab,
+			             transform.position - transform.forward,
+			             Quaternion.identity);
+
+		// make the bullet move away in front of the player
+		bullet.GetComponent<Rigidbody>().velocity = -transform.forward * 4;
+
+		// spawn the bullet on the clients
+		NetworkServer.Spawn(bullet);
+
+		// make bullet disappear after 2 seconds
+		Destroy(bullet, 2.0f);
+	}
+
+	public override void OnStartLocalPlayer()
+	{
+		GetComponent<MeshRenderer> ().material.color = Color.red;
+	}
+
+
+}
