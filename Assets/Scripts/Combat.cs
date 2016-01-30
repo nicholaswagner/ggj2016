c9750399f97027b7a45e29c6dd45a39d22a391c5
Nicholas Wagner <tinker@hackbookpro.local>
2016-01-30 11:06:13 -0600

    adds networked player helath and client respawn rpc

--- Assets/Scripts/Combat.cs
+++ Assets/Scripts/Combat.cs
@@ -1,27 +1,32 @@
 ﻿using UnityEngine;
 using System.Collections;
+using UnityEngine.Networking;
 
-public class Combat : MonoBehaviour {
+
+public class Combat : NetworkBehaviour {
 
 	public const int maxHealth = 100;
+
+	[SyncVar]
 	public int health = maxHealth;
 
 	public void TakeDamage(int amount)
 	{
 		health -= amount;
 		if (health <= 0) {
-			health = 0;
-			Debug.Log ("DEAD!");
+			Debug.Log ("YOU DEAD!");
+			RpcRespawn();
+			health = maxHealth;
 		}
 	}
 
-	// Use this for initialization
-	void Start () {
-	
+	[ClientRpc]
+	void RpcRespawn()
+	{
+		if (isLocalPlayer) {
+			// move back to zero location
+			transform.position = Vector3.zero;
+		}
 	}
 	
-	// Update is called once per frame
-	void Update () {
-	
-	}
 }
