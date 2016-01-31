5b646d015e374e33da7e8152a375470788cc6989
Nicholas Wagner <nickgwagner@gmail.com>
2016-01-31 12:50:21 -0600

    adds duct tape font, unconnected screen, and some silly scene button

---
+++ Assets/UnconnectedScreenManager.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class UnconnectedScreenManager : MonoBehaviour {
+
+	public void HandleConnectButtonPress () {
+		Debug.Log ("You Pressed the button");
+	}
+}
